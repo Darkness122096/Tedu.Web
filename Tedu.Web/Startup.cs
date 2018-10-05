29207e7fbf9c1f44094548aa89a14e1192440c7c
Darkness122096 <nguyentranthienhuy@gmail.com>
2018-10-05 21:34:14 +0700

    first commit

---
+++ Tedu.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tedu.Web.Startup))]
+namespace Tedu.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
